086b65607c69df702c81f6c797cba750fd0e142b
Wxx <1191462745@qq.com>
2017-08-05 14:29:00 +0800

    加入创建数据和和表的各项配置

---
+++ app/src/main/java/com/example/coolwheather/db/Country.java
@@ -0,0 +1,46 @@
+package com.example.coolwheather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by magin on 2017/8/5.
+ */
+
+public class Country extends DataSupport {
+    private int id;
+    private String countryName;
+    private String weatherId;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
